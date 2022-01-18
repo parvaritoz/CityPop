fd06a68fe13a5b651dbd9e295658c0a373e2872c
Reza Parvar <id18rpr@cs.umu.se>
2022-01-18 17:29:59 +0100

    Started with bacis layouts for homeScreen.

--- App.js
+++ App.js
@@ -1,25 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import HomeScreen from './screens/homeScreen';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import HomeScreen from "./screens/HomeScreen";
 
 export default function App() {
-  return (
-      <View style={styles.container}>
-        <Text style={styles.text}>CityPop</Text>
-      </View>
-  );
+  return <HomeScreen></HomeScreen>;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-
-  },
-  text: {
-    fontSize: 35,
-    fontWeight: 'bold',
-    marginTop: '60%',    
-  },
-});
