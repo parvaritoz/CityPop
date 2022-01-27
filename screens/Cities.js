0dda81c798f6e4ebedc295ba1d7b31a2883f899b
Reza Parvar <id18rpr@cs.umu.se>
2022-01-27 13:18:51 +0100

    Fetch data shown in screen for searchByCountry.

--- screens/Cities.js
+++ screens/Cities.js
@@ -4,16 +4,26 @@ import CityButton from "../components/CityButton";
 import colors from "../config/colors";
 
 export default function Cities({ navigation }) {
-    const data = navigation.state.params;
-    console.log(navigation);
-    return (
-        <View style={styles.container}>
-            <Text style={styles.cityText}>France</Text>
-            <CityButton title="Paris"></CityButton>
-            <CityButton title="Paris"></CityButton>
-            <CityButton title="Paris"></CityButton>
-        </View>
-    );
+  const data = navigation.state.params;
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.cityText}>{data[0].countryName}</Text>
+
+      <CityButton
+        title={data[0].toponymName}
+        onPress={() => navigation.navigate("CityPopulation", data[0])}
+      ></CityButton>
+      <CityButton
+        title={data[1].toponymName}
+        onPress={() => navigation.navigate("CityPopulation", data[1])}
+      ></CityButton>
+      <CityButton
+        title={data[2].toponymName}
+        onPress={() => navigation.navigate("CityPopulation", data[2])}
+      ></CityButton>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
